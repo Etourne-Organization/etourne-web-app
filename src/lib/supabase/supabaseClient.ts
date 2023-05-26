bdbe837109bafea3458ee01d8d642ab138bc4825
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-05-26 12:32:07 +0100

    update supabaseClient

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -1,9 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
 import { env } from '$env/dynamic/public';
 
-export const TEST = import.meta.env.VITE_TEST;
-
-// import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/public';
+import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL: string | any = env.PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY: string | any = env.PUBLIC_SUPABASE_ANON_KEY;
