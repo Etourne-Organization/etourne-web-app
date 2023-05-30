353435ade1dcee0e8099f20bba6b0e99c5b996dd
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-05-30 16:53:28 +0100

    testing playwright

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -7,23 +7,9 @@ test('has title', async ({ page }) => {
 	await expect(page).toHaveTitle(/Etourne/);
 });
 
-// test('get started link', async ({ page }) => {
-// 	await page.goto('https://playwright.dev/');
-
-// 	// Click the get started link.
-// 	await page.getByRole('link', { name: 'Get started' }).click();
-
-// 	// Expects the URL to contain intro.
-// 	await expect(page).toHaveURL(/.*intro/);
-// });
-
 test('check sign in with discord button exists', async ({ page }) => {
 	await page.goto('http://127.0.0.1:5173/');
 
-	// get
+	// check if sign in button exists in the page
 	await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
-	// await page.getByRole('link', { name: 'Sign In' });
-
-	// Expects the URL to contain intro.
-	// await expect(page).toHaveURL(/.*intro/);
 });
