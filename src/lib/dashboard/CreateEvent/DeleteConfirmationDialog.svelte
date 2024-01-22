de07794d6a603d18f6c8090ea2765bc0b41e530b
noob_dev54 <muntasirenamalhuda@gmail.com>
2024-01-22 18:20:25 +0000

    still trying to fix the bug with delete event dialog

---
+++ src/lib/dashboard/CreateEvent/DeleteConfirmationDialog.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let eventId: number;
+	export let isDialogOpen: boolean;
+
+	console.log(isDialogOpen);
+
+	import { fade } from 'svelte/transition';
+
+	import { Dialog, Separator, Label, DropdownMenu, Button } from 'bits-ui';
+</script>
+
+<Dialog.Root bind:open={isDialogOpen}>
+	<Dialog.Trigger />
+
+	<Dialog.Portal>
+		<Dialog.Overlay
+			transition={fade}
+			transitionConfig={{ duration: 150 }}
+			class="fixed inset-0 z-50 backdrop-blur-sm"
+		/>
+		<Dialog.Content
+			class="fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] rounded-lg border p-5 outline-none bg-secondary border-none sm:max-w-[490px] md:w-full"
+		>
+			<Dialog.Title
+				class="flex w-full items-center justify-center text-lg font-semibold tracking-tight text-white"
+			>
+				Are you sure you want to delete this event?
+			</Dialog.Title>
+			<Separator.Root class="-mx-5 mb-6 mt-5 block h-px bg-gray-400" />
+			<div class="flex gap-3">
+				<Dialog.Close>
+					<button class="btn btn-primary">Delete</button>
+				</Dialog.Close>
+				<Dialog.Close>
+					<button class="btn btn-neutral">Delete</button>
+				</Dialog.Close>
+			</div>
+		</Dialog.Content>
+	</Dialog.Portal>
+</Dialog.Root>
--- src/routes/dashboard/[id=integer]/all-events/+page.svelte
+++ src/routes/dashboard/[id=integer]/all-events/+page.svelte
@@ -3,14 +3,15 @@
 
 	const { allEvents } = data;
 
-	import { fade } from 'svelte/transition';
+	import { DropdownMenu, Button } from 'bits-ui';
 
-	import { Dialog, Separator, Label, DropdownMenu, Button } from 'bits-ui';
+	import DeleteConfirmationDialog from '$lib/dashboard/CreateEvent/DeleteConfirmationDialog.svelte';
 
-	let isDialogOpen: boolean = false;
+	let currentDialogOpenId: number | null = null;
 
-	const onDeleteButtonClick = () => {
-		isDialogOpen = true;
+	const onDeleteButtonClick = (eventId: number) => {
+		currentDialogOpenId = eventId;
+		console.log(currentDialogOpenId);
 	};
 </script>
 
@@ -82,7 +83,7 @@
 
 									<DropdownMenu.Item
 										class="h-10 flex items-center gap-2 rounded-md py-3 px-3 font-medium hover:cursor-pointer hover:bg-[#4d0f0f]"
-										on:click={onDeleteButtonClick}
+										on:click={() => onDeleteButtonClick(aE.id)}
 									>
 										<i class="ri-delete-bin-fill" />
 										Delete
@@ -90,37 +91,12 @@
 								</DropdownMenu.Content>
 							</DropdownMenu.Root>
 
-							<Dialog.Root bind:open={isDialogOpen}>
-								<Dialog.Portal>
-									<Dialog.Overlay
-										transition={fade}
-										transitionConfig={{ duration: 150 }}
-										class="fixed inset-0 z-50 backdrop-blur-sm"
-									/>
-									<Dialog.Content
-										class="fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] rounded-lg border p-5 outline-none bg-secondary border-none sm:max-w-[490px] md:w-full"
-									>
-										<Dialog.Title
-											class="flex w-full items-center justify-center text-lg font-semibold tracking-tight text-white"
-											>Are you sure you want to delete this event?</Dialog.Title
-										>
-										<Separator.Root
-											class="-mx-5 mb-6 mt-5 block h-px bg-gray-400"
-										/>
-										<div class="flex gap-3">
-											<Dialog.Close>
-												<Button.Root
-													class="inline-flex h-12 items-center justify-center rounded-input bg-white
-  px-[21px] text-[15px] font-semibold text-background shadow-mini
-  hover:bg-dark/95 active:scale-98 active:transition-all"
-												>
-													Unlimited
-												</Button.Root>
-											</Dialog.Close>
-										</div>
-									</Dialog.Content>
-								</Dialog.Portal>
-							</Dialog.Root>
+							<DeleteConfirmationDialog
+								eventId={aE.id}
+								isDialogOpen={currentDialogOpenId === aE.id
+									? true
+									: false}
+							/>
 						</td>
 					</tr>
 				{/each}
